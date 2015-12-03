bd46e1214cd54b1f38f04abb0112b6300e27a05d
Scott Lavigne <scottlavigne28@gmail.com>
2015-12-03 03:31:17 +0000

    Added admin link to footer if admin logged in, admins can only see links of their admin level or lower.

--- app/views/admin/index.blade.php
+++ app/views/admin/index.blade.php
@@ -4,7 +4,9 @@
 @stop
 
 @section('content')
-<a href="{{URL::to('dic/addnew')}}">Add New Word</a><br>
-<a href="{{URL::to('dic/review')}}">Review Words by Language</a><br>
-<a href="{{URL::to('dic/levelselect')}}">Review Words by Level</a><br>
+@if(Auth::check() && Auth::user()->level >= 101)
+    <a href="{{URL::to('dic/addnew')}}">Add New Word</a><br>
+    <a href="{{URL::to('dic/review')}}">Review Words by Language</a><br>
+    <a href="{{URL::to('dic/levelselect')}}">Review Words by Level</a><br>
+@endif
 @stop
