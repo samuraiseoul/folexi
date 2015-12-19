e1bf4b4f77706226f171a7c029d8482d2437ebf6
Scott Lavigne <scottlavigne28@gmail.com>
2015-12-19 02:41:30 +0000

    404 page css done

--- app/views/404/index.blade.php
+++ app/views/404/index.blade.php
@@ -4,12 +4,16 @@
 @stop
 
 @section('content')
-<h1>ERROR 404</h1>
-<div class="about_container">
-    <span class="text">
-        Looks like you're looking for a page that doesn't exist!
-        If you feel you have reached this page in error,
-        please feel free to contact us a folexi.info@gmail.com!
-    </span>
+<div class="row error_page">
+    <div class="col_12">
+        <div class="text_container">
+            <h1>ERROR 404</h1>
+            <span class="text">
+                Looks like you're looking for a page that doesn't exist!
+                If you feel you have reached this page in error,
+                please feel free to contact us a folexi.info@gmail.com!
+            </span>
+        </div>
+    </div>
 </div>
 @stop
