2adb114575390097056dfe0baa128a99e092df00
GlixG <philipgilley@gmail.com>
2016-01-26 12:52:21 -0600

    added the explanation page

---
+++ app/views/info/explanation.blade.php
@@ -0,0 +1,24 @@
+@extends('template.template')
+
+@section('content')
+<article id="explanation">
+    <div class="row">
+        <div class="col_12 last">
+            <h1>Explanation Page</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="offset col_2"></div>
+        <div class="about_container col_8">
+            <span class="text">
+                <h3>Why FOLEXI?</h3>
+                
+                <p>
+                    Because I said so.
+                </p>
+            </span>
+        </div>
+        <div class="offset col_2 last"></div>
+    </div>
+</article>
+@stop
